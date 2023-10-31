cf28d53f20a8ccee03dfecb548ea627f088cd138
Derek Sudduth <suddd@amazon.com>
2023-10-31 02:55:00 -0700

    Removes env vars

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -4,11 +4,5 @@ import * as cdk from 'aws-cdk-lib';
 import { DeploymentStack } from '../lib/deployment-stack';
 
 const app = new cdk.App();
-const env = {
-  region: process.env.CDK_DEPLOY_ACCOUNT ?? process.env.CDK_DEFAULT_ACCOUNT,
-  account: process.env.CDK_DEPLOY_REGION ?? process.env.CDK_DEFAULT_REGION
-}
 
-new DeploymentStack(app, 'DeploymentStack', {
-  env: env
-});
+new DeploymentStack(app, 'DeploymentStack', {});
