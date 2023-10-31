0bb8437120c11e6bdd54fc499a5304e5a9064815
Derek Sudduth <suddd@amazon.com>
2023-10-31 02:39:59 -0700

    Adds cloudfront and route53

--- deployment/lib/deployment-stack.ts
+++ deployment/lib/deployment-stack.ts
@@ -2,13 +2,20 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import { OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { OriginAccessIdentity, Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 
 export class DeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const distPath = 'src/docs/.vuepress/dist'
+    const accountId = cdk.Aws.ACCOUNT_ID
+    const certificate = Certificate.fromCertificateArn(this, 'FiftyOneFiftyCertificate', `arn:aws:acm:us-east-1:${accountId}:certificate/e2c31c19-7d2e-4009-882e-0fc10730238d`)
+    const hostedZoneName = 'fiftyonefifty.dev'
 
     const bucket = new Bucket(this, 'FiftyOneFiftyBucket', {
       blockPublicAccess: {
@@ -31,5 +38,24 @@ export class DeploymentStack extends cdk.Stack {
     })
     bucket.grantRead(oai)
 
+    const distribution = new Distribution(this, 'FiftyOneFiftyDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(bucket, {
+          originAccessIdentity: oai
+        }),
+      },
+      enabled: true,
+      defaultRootObject: 'index.html',
+      domainNames: [hostedZoneName],
+      certificate: certificate
+    })
+
+    new ARecord(this, 'FiftyOneFiftyARecord', {
+      zone: HostedZone.fromLookup(this, 'FiftyOneFiftyHostedZone', {
+        domainName: hostedZoneName
+      }),
+      recordName: hostedZoneName,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    })
   }
 }
