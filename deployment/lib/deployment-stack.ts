42805cadbea236539f2ba00babcf628e05204bfb
Derek Sudduth <suddd@amazon.com>
2023-10-31 02:22:46 -0700

    Adds OAI

--- deployment/lib/deployment-stack.ts
+++ deployment/lib/deployment-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
 
 export class DeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -18,5 +20,16 @@ export class DeploymentStack extends cdk.Stack {
       autoDeleteObjects: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     })
+
+    new BucketDeployment(this, 'FiftyOneFiftyBucketDeployment', {
+      sources: [Source.asset(`../${distPath}`)],
+      destinationBucket: bucket
+    })
+
+    const oai = new OriginAccessIdentity(this, 'FiftyOneFiftyOAI', {
+      comment: 'FiftyOneFifty OAI'
+    })
+    bucket.grantRead(oai)
+
   }
 }
