c440ca76030c171c4e72e868888eef80e7608e01
Derek Sudduth <suddd@amazon.com>
2023-10-31 03:04:16 -0700

    Add env

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -4,5 +4,12 @@ import * as cdk from 'aws-cdk-lib';
 import { DeploymentStack } from '../lib/deployment-stack';
 
 const app = new cdk.App();
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION
+};
 
-new DeploymentStack(app, 'DeploymentStack', {});
+
+new DeploymentStack(app, 'DeploymentStack', {
+  env: env
+});
